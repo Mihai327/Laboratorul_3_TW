14e2d784d4e77f0de2d911016f463b705c5b970b
Mihai327 <62377695+Mihai327@users.noreply.github.com>
2020-05-06 20:58:53 +0300

    de la labor.3

---
+++ eUseControl.BusinessLogic/MyBusinessLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Top_Infinity.BusinessLogic.Interfaces;
+
+namespace Top_Infinity.BusinessLogic
+{
+    public class MyBusinessLogic
+    {
+        public ISession GetSessionBL()
+        {
+            return new SessionBL();
+        }
+    }
+}
---
+++ eUseControl.BusinessLogic/SessionBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Top_Infinity.BusinessLogic.Core;
+using Top_Infinity.BusinessLogic.Interfaces;
+using Top_Infinity.Domain.Entities.User;
+
+namespace Top_Infinity.BusinessLogic
+{
+    class SessionBL: UserApi, ISession
+    {
+        public ULoginResp UserLogin(ULoginData data)
+        {
+            return new ULoginResp();
+        }
+    }
+}
